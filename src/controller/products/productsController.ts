e2780241d2d148721f0b405435188f0a63c37c41
Nd Morsalin <121650325+ND-Morsalin@users.noreply.github.com>
2024-04-21 12:37:22 +0600

    Update routes.ts and productsController.ts with improved formatting and error handling

--- src/controller/products/productsController.ts
+++ src/controller/products/productsController.ts
@@ -1,14 +1,198 @@
+import { Product } from "@prisma/client";
 import { Request, Response } from "express";
+import prisma from "../../utility/prisma";
 
-const addProduct = async (req: Request, res: Response) => {};
+const addProduct = async (req: Request, res: Response) => {
+  try {
+    const {
+      productName,
+      amount,
+      buyingPrice,
+      sellingPrice,
+      productCategory,
+      productBrand,
+      unit,
+      shopOwnerId,
+    } = req.body as Product;
 
-const getAllProducts = async (req: Request, res: Response) => {};
+    const product = await prisma.product.create({
+      data: {
+        productName,
+        amount,
+        buyingPrice,
+        sellingPrice,
+        productCategory,
+        productBrand,
+        unit,
+        shopOwnerId,
+      },
+    });
 
-const getSingleProduct = async (req: Request, res: Response) => {};
+    return res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "addProduct function",
+        },
+      ],
+    });
+  }
+};
 
-const updateProduct = async (req: Request, res: Response) => {};
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await prisma.product.findMany();
 
-const deleteProduct = async (req: Request, res: Response) => {};
+    return res.status(200).json({
+      success: true,
+      message: "All products",
+      products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "getAllProducts function",
+        },
+      ],
+    });
+  }
+};
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id: id as string,
+      },
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        errors: [
+          {
+            type: "not found",
+            value: id,
+            msg: "Product not found",
+            path: "product",
+            location: "getSingleProduct function",
+          },
+        ],
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product found",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "getSingleProduct function",
+        },
+      ],
+    });
+  }
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    // ! update only amount, buyingPrice, sellingPrice, unit
+    const { amount, buyingPrice, sellingPrice, unit, shopOwnerId } =
+      req.body as Product;
+
+    const product = await prisma.product.update({
+      where: {
+        id,
+        shopOwnerId,
+      },
+      data: {
+        amount,
+        buyingPrice,
+        sellingPrice,
+        unit,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "updateProduct function",
+        },
+      ],
+    });
+  }
+};
+
+const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { shopOwnerId } = req.body;
+
+    const deletedProduct = await prisma.product.delete({
+      where: {
+        id,
+        shopOwnerId: shopOwnerId as string,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      deletedProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "deleteProduct function",
+        },
+      ],
+    });
+  }
+};
 
 export {
   addProduct,
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,7 +7,13 @@ import shopOwnerBodyChecker from "../middleware/shopOwner/shopOwnerValidator";
 import handleValidationErrors from "../middleware/handelValidatorError";
 import logInValidator from "../middleware/shopOwner/loginValidator";
 import checkValidUser from "../middleware/checkValidUser";
-import { addProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from "../controller/products/productsController";
+import {
+  addProduct,
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+} from "../controller/products/productsController";
 import productBodyChecker from "../middleware/products/productValidator";
 
 const router = Router();
@@ -38,19 +44,25 @@ router.get("/", checkValidUser, (req, res) => {
  **/
 
 // create product
-router.post("/product", checkValidUser,productBodyChecker,handleValidationErrors, addProduct);
+router.post(
+  "/product",
+  checkValidUser,
+  productBodyChecker,
+  handleValidationErrors,
+  addProduct
+);
 
 // get all products
 router.get("/products", checkValidUser, getAllProducts);
 
 // get single product
-router.get("/product/:id",checkValidUser, getSingleProduct);
+router.get("/product/:id", checkValidUser, getSingleProduct);
 
 // update product
-router.put("/product/:id",checkValidUser, updateProduct);
+router.put("/product/:id", checkValidUser, updateProduct);
 
 // delete product
-router.delete("/product/:id",checkValidUser, deleteProduct);
+router.delete("/product/:id", checkValidUser, deleteProduct);
 
 /**
  * PRODUCT ROUTES end
