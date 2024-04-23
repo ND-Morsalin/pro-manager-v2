b53ceb405b8328186bb593798bfa14b519ca45a7
Nd Morsalin <121650325+ND-Morsalin@users.noreply.github.com>
2024-04-23 21:09:59 +0600

    Fix typo in swagger.yaml for /api/lone-provider route

---
+++ src/controller/businessContactInfo/businessContactInfo.ts
@@ -0,0 +1,10 @@
+import { Response } from "express";
+import { ExtendedRequest } from "../../types/types";
+
+const createBusinessContactInfo = async (req:ExtendedRequest,res:Response)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
