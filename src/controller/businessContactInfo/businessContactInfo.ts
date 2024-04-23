3b3d3bf4de045377052c95a39b557880e155469c
Nd Morsalin <121650325+ND-Morsalin@users.noreply.github.com>
2024-04-23 21:43:22 +0600

    Add business contact info routes and controller functions

--- src/controller/businessContactInfo/businessContactInfo.ts
+++ src/controller/businessContactInfo/businessContactInfo.ts
@@ -1,10 +1,148 @@
 import { Response } from "express";
 import { ExtendedRequest } from "../../types/types";
+import { BusinessContactInfo } from "@prisma/client";
+import prisma from "utility/prisma";
 
-const createBusinessContactInfo = async (req:ExtendedRequest,res:Response)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const createBusinessContactInfo = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+    const businessInfo = req.body as BusinessContactInfo;
+
+    const businessContactInfo = await prisma.businessContactInfo.create({
+      data: {
+        shopOwnerId: req.shopOwner.id,
+        name: businessInfo.name,
+        organization: businessInfo.organization,
+        phoneNumber: businessInfo.phoneNumber,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "business Contact info created",
+      businessContactInfo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "createBusinessContactInfo",
+        },
+      ],
+    });
+  }
+};
+
+const getAllBusinessContactInfo = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+
+    const allBusinessContactInfo = await prisma.businessContactInfo.findMany({
+        where:{
+            shopOwnerId:req.shopOwner.id
+        }
+    })
+
+    return res.status(200).json({
+        success:true,
+        allBusinessContactInfo,
+        message :'all business info'
+    })
+
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "getAllBusinessContactInfo",
+        },
+      ],
+    });
+  }
+};
+
+const getSingleBusinessContactInfo = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "getSingleBusinessContactInfo",
+        },
+      ],
+    });
+  }
+};
+
+const updateBusinessContactInfo = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "updateBusinessContactInfo",
+        },
+      ],
+    });
+  }
+};
+
+const deleteBusinessContactInfo = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          type: "server error",
+          value: "",
+          msg: "Internal server error",
+          path: "server",
+          location: "deleteBusinessContactInfo",
+        },
+      ],
+    });
+  }
+};
+
+export {
+  createBusinessContactInfo,
+  getAllBusinessContactInfo,
+  getSingleBusinessContactInfo,
+  updateBusinessContactInfo,
+  deleteBusinessContactInfo,
+};
