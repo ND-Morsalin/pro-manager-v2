3cdf11b7db01450487f889c9e072e30fd176cd55
Nd-Morsalin <ndmorsalin99@gmail.com>
2024-09-04 21:52:23 +0600

    Unsuccessfull!! Balance Validity Not Available in this ndmorsalin Account

--- src/utility/purchaseConfirmBySms.ts
+++ src/utility/purchaseConfirmBySms.ts
@@ -14,19 +14,26 @@ const purchaseConfirmBySms = async ({
 })=>{
 // at first check if the user exists
 
-const smsPost = await axios.get(
-    `http://bulksmsbd.net/api/smsapi?api_key=3CuemU2YW4dCNqDJulbJ&type=text&number=88${mobile}&senderid=8809617618303&message=Your Manager.com 
-    দোকানের নাম: ${shopName}
-    মোট পণ্য ক্রয়: ${totalAmount} টাকা
-    বকেয়া আছে: ${dueAmount} টাকা
-    `
-  );
-
-  console.log({
+try {
+    const smsPost = await axios.get(
+        `http://bulksmsbd.net/api/smsapi?api_key=3CuemU2YW4dCNqDJulbJ&type=text&number=88${'01646653053'}&senderid=8809617618303&message=Your Manager.com 
+        দোকানের নাম: ${shopName}
+        মোট পণ্য ক্রয়: ${totalAmount} টাকা
+        বকেয়া আছে: ${dueAmount} টাকা
+        `
+      );
+     console.log({
       data: smsPost.data,
-    smsPost,
   })
 
+} catch (error) {
+
+    console.log({
+        error
+    })
+    
+}
+ 
 }
 
 export default purchaseConfirmBySms;
